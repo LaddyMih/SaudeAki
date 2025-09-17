71cedc6e0221f331bb6cba1d136faa97dd250853
Yasmin Evelyn Roder <00001099069786sp@aluno.educacao.sp.gov.br>
2025-09-17 11:25:45 -0300

    feat:criando um banco de dados e integrando ele mongodb e prisma

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,37 +1,12 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { serialize, parse } from "cookie";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+import { PrismaClient } from "@prisma/client";
 
-const JWT_SECRET = "SUA_CHAVE_SECRETA"; // coloque algo forte
+const prisma = new PrismaClient();
 
-export async function hashPassword(password: string) {
-  return await bcrypt.hash(password, 10);
-}
-
-export async function verifyPassword(password: string, hash: string) {
-  return await bcrypt.compare(password, hash);
-}
-
-export function createToken(payload: object) {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
-}
-
-export function verifyToken(token: string) {
-  try {
-    return jwt.verify(token, JWT_SECRET);
-  } catch {
-    return null;
-  }
-}
-
-export function setTokenCookie(token: string) {
-  return serialize("token", token, {
-    httpOnly: true,
-    path: "/",
-    maxAge: 60 * 60 * 24 * 7, // 7 dias
-  });
-}
-
-export function parseCookies(cookieHeader: string | undefined) {
-  return cookieHeader ? parse(cookieHeader) : {};
-}
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "mongodb", // or "mysql", "postgresql", ...etc
+    }),
+});
